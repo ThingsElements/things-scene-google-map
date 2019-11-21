65f00e414a899d1d2efe582b09f28db9e1bcd346
EunHyeYun <lillyeh@hatiolab.com>
2019-11-21 18:07:07 +0900

    Revert "buildMarker에서 _markerComponents가 없을 경우"
    This reverts commit 090e28b03a501cf2cfccf1802d3cf7b6abb86e09.

--- src/google-map.js
+++ src/google-map.js
@@ -182,22 +182,22 @@ export default class GoogleMap extends HTMLOverlayContainer {
 
   buildMarkers() {
     var markers = [];
-    if (this._markerComponents) {
-      this._markerComponents.forEach(component => {
-        let { lat, lng } = component.model;
 
-        let marker = new google.maps.Marker({
-          position: {
-            lat,
-            lng
-          },
-          map: this.map
-        });
-        markers.push(marker);
+    this._markerComponents.forEach(component => {
+      let { lat, lng } = component.model;
 
-        component.marker = marker;
+      let marker = new google.maps.Marker({
+        position: {
+          lat,
+          lng
+        },
+        map: this.map
       });
-    }
+      markers.push(marker);
+
+      component.marker = marker;
+    });
+
     this._markers = markers;
   }
 
